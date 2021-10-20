08f7e941d19a939961700ca2236535d681f5aed8
赤堀竜海 <tatsumiakahori@tummy.local>
2021-10-20 21:46:01 +0900

    背景に設定できるか検証

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,32 @@
+var noiseScale = 800;
+var canvas;
+
+/*
+ * ウィンドウの大きさが変更されたときに背景が再度描画されるように
+ * 元々setup()にあった処理をここに移動した
+ */
+function canvasSetup(){
+    background(21, 8, 50);
+    for(var i = 0; i < nums; i++){
+        particles_a[i] = new Particle(random(0, width),random(0,height));
+        particles_b[i] = new Particle(random(0, width),random(0,height));
+        particles_c[i] = new Particle(random(0, width),random(0,height));
+    }
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+    canvasSetup();
+}
+
 function setup() {
-    createCanvas(400, 400);
-  }
-  
-  function draw() {
+    canvas = createCanvas(windowWidth, windowHeight);//ブラウザのウィンドウサイズに合わせてcanvas作成
+    canvas.style('z-index','-1');//canvasを後ろに移動する。
+
+    canvasSetup();
+}
+
+function draw() {
     background(230);
-  
     circle(200, 200, 150, 150);
-  }
+}
